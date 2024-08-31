1b08d990b54bff56dfb8a658bbbb6ddb3bbf9dbe
harshpratik2411 <harshsrivastava2357@gmail.com>
2024-08-31 21:02:35 +0530

    new data types summary

---
+++ 01_basics/04_datatypes_summary.js
@@ -0,0 +1,37 @@
+// DATA TYPES
+// 
+//   primitive data type 
+//    there are sven types of primitive data 
+//     string,number,boolean,null,undefined,symbol,bigInt
+// 
+//   Refrence type or non primitive data type 
+//   arrays ,objects, functions 
+//
+    const heros =["captain","stark","vison"];
+    
+    let myObj = {
+        name:"harsh",
+        age:21
+     }
+     const myFunction = function(){
+        console.log("hello");
+        
+     }  
+    //   All return types of data type in js 
+    
+    //  primitive return type
+
+    // Number => number
+    //  String  => string
+    //  Boolean  => boolean
+    //  null  => object
+    //  undefined  =>  undefined
+    //  Symbol  =>  symbol
+    //  BigInt  =>  bigint
+       
+    // Non-primitive Datatypes
+
+    // Arrays  =>  object
+    // Function  =>  function
+    // Object  =>  object
+

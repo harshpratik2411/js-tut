9f49c59c7fbfc6f315b36e5495d701994e074fe4
harshpratik2411 <harshsrivastava2357@gmail.com>
2024-08-31 20:16:33 +0530

    01_basics

--- 01-basics/03_conversion.js
+++
@@ -1,3 +0,0 @@
-let strInt = "123";
-let numInt = parseInt(strInt, 10);
-console.log(numInt);
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,15 @@
+let strInt = "123";
+let numInt = parseInt(strInt, 10);
+console.log(numInt);
+
+
+ let str1 = "harsh"
+  let str2 = "raj"
+ let str3 = str1+str2;
+ console.log(str3);
+  
+console.log(1 +"2"); 
+console.log("1"+ 2);
+console.log("1"+ 2 + 2);
+console.log(2 + 1 +"1");
+

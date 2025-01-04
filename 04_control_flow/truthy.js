f84087b3d13a2b44f1166f69041876c496f5012b
harshpratik2411 <harshsrivastava2357@gmail.com>
2025-01-04 21:13:52 +0530

    new notes added

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,24 @@
+//   const userEmail =  "hiiii"
+
+//   if (userEmail) {
+//      console.log("got user email");
+     
+//   } else {
+//     console.log("dont have user email");
+    
+//   } 
+
+
+// falsy values  
+
+//examples
+// false,0,-0,BigInt 0n,"",null,undefined, NaN 
+
+// truthy values
+
+//examples
+"0"  // if the zero is inside the string it is truthy value
+'false' ,
+ " " // if there is any value in string it is truthy value which includes empty  space also
+// [],{} , function(){} ,
+ 
---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// let index = 0
+// while (index <=20) {
+//    console.log(`value of index ${index}`);
+//    index = index+2;
+    
+// } 
+
+// let Myarr=["flash","wonder","batman","superman"] 
+// let arr = 0
+// while (arr < Myarr.length) {
+//     console.log(`the value of arr is ${Myarr[arr]}`);
+//      arr = arr+1;
+// }
+ 
+let score = 1
+do {
+    console.log(`the score is ${score}`);
+     score ++
+} while (score <=14);
